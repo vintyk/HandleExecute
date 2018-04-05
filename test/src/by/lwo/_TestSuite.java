b6b186ca5d8c6f66a1d89748277df0dc1de8b4d3
vinty1978@gmail.com <djkxtyjr3>
2018-04-05 16:45:38 +0300

    Мы что-то поменяли.

---
+++ test/src/by/lwo/_TestSuite.java
@@ -0,0 +1,12 @@
+package src.by.lwo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+StartCircleIT.class
+})
+public class _TestSuite {
+}
+
