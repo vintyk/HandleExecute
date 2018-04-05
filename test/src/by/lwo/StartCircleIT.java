d52b334a95bd1d8683174d207a42d81db4ecedaa
vinty1978@gmail.com <djkxtyjr3>
2018-04-05 14:11:10 +0300

    commit из NetBeans 2

--- test/src/by/lwo/StartCircleIT.java
+++ test/src/by/lwo/StartCircleIT.java
@@ -1,5 +1,6 @@
 package src.by.lwo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,6 +10,18 @@ public class StartCircleIT {
     @Test
     public void testMain() {
         System.out.println("----- Тест запустил какую-то процедуру -----");
+        assertEquals(1, 1);
+    }
+    @Test
+    public void testWow() {
+        System.out.println("----- Тест запустил еще какую-то процедуру -----");
+        Assert.assertEquals("Тут описание какого то сравнения", 1, 1);
+        assertEquals(1, 1);
+    }
+    @Test
+    public void testGetSmth() {
+        System.out.println("----- Тест запустил опять и снова какую-то процедуру -----");
+        Assert.assertEquals("Тут описание еще одного сравнения", 1, 1);
         assertEquals(1, 1);
     }
 }
