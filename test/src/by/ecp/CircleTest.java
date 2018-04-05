7c2e0c0996be55d83c658714933edd466ebfbae9
vinty1978@gmail.com <djkxtyjr3>
2018-04-05 17:16:10 +0300

    + 1 тест

---
+++ test/src/by/ecp/CircleTest.java
@@ -0,0 +1,16 @@
+package src.by.ecp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CircleTest {
+    @Test
+    public void testGetRadius() {
+        System.out.println("getRadius");
+        Circle instance = null;
+        double expResult = 0.0;
+        double result = instance.getRadius();
+        assertEquals(1, 1);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+}
